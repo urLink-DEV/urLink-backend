b612049867ff809e14a88e2215f6d1ff027b0251
조홍제 <chj@wemakeprice.com>
2020-12-08 22:40:14 +0900

    image path가 올바르지 않을 때 404 처리 - 2020.12.08

--- server/v1/url/utils/crawler.py
+++ server/v1/url/utils/crawler.py
@@ -37,14 +37,33 @@ class Crawler:
                 return html.find('link', key).get('href')
         return '알수없음'
 
-    def is_correct_image(self, image_path):
-        response = requests.get(image_path)
+    def is_correct_image(self, path):
         try:
+            response = requests.get(path)
             if "image" in response.headers['Content-Type']:
                 return True
         except Exception as e:
             print(e)
         return False
+
+    def is_unknown_image_or_favicon(self, type, path):
+        if path == "알수없음":
+            if type == 'image':
+                return self.IMAGE_404
+            else:
+                return self.IMAGE_FAVICON
+        else:
+            return path
+
+    def is_insufficient_url(self, path):
+        if urlparse(path).scheme == '':
+            url = urlparse(self.path)
+            if urlparse(path).netloc == '':
+                return f"{url.scheme}://{url.netloc}{path}"
+            else:
+                return f"{url.scheme}:{path}"
+        else:
+            return path
 
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
@@ -63,26 +82,15 @@ class Crawler:
         else:
             title = self.get_meta_data(head, [{'name': 'title'}, {'property': 'og:title'}])
         description = self.get_meta_data(head, [{'name': 'description'}, {'property': 'og:description'}])
-        image_path = self.get_meta_data(head, [{'property': 'og:image'}, {'property': 'twitter:image'}])
-        favicon_path = self.get_favicon(head, [{'rel': 'shortcut icon'}, {'rel': 'icon'}])
+        image_path = self.is_insufficient_url(self.is_unknown_image_or_favicon('image', self.get_meta_data(head, [
+            {'property': 'og:image'}, {'property': 'twitter:image'}])))
+        favicon_path = self.is_insufficient_url(self.is_unknown_image_or_favicon('favicon', self.get_favicon(head, [
+            {'rel': 'shortcut icon'}, {'rel': 'icon'}])))
 
-        if image_path == '알수없음' or not self.is_correct_image(image_path):
-            image_path = self.IMAGE_404
-        if urlparse(image_path).scheme == '' and urlparse(image_path).netloc == '':
-            url = urlparse(self.path)
-            image_path = f"{url.scheme}://{url.netloc}{image_path}"
-        elif urlparse(image_path).scheme == '' and urlparse(image_path).netloc != '':
-            url = urlparse(self.path)
-            image_path = f"{url.scheme}:{image_path}"
-
-        if favicon_path == '알수없음':
-            favicon_path = self.IMAGE_FAVICON
-        if urlparse(favicon_path).scheme == '' and urlparse(favicon_path).netloc == '':
-            url = urlparse(self.path)
-            favicon_path = f"{url.scheme}://{url.netloc}{favicon_path}"
-        elif urlparse(favicon_path).scheme == '' and urlparse(favicon_path).netloc != '':
-            url = urlparse(self.path)
-            favicon_path = f"{url.scheme}:{favicon_path}"
+        if not self.is_correct_image(image_path):
+            image_path = self.is_unknown_image_or_favicon('image', '알수없음')
+        if not self.is_correct_image(favicon_path):
+            favicon_path = self.is_unknown_image_or_favicon('favicon', '알수없음')
 
         return {
             'title': title,
@@ -96,6 +104,18 @@ if __name__ == "__main__":
     from pprint import pprint
 
     c = Crawler()
-    for path in ['https://www.kobaco.co.kr/site/main/content/what_public_ad']:
+    for path in ['abc',
+                 'https://programmers.co.kr/learn/challenges?tab=all_challenges',
+                 'https://www.acmicpc.net/',
+                 'https://ssungkang.tistory.com/category/%EC%9B%B9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/Django',
+                 'https://tech.cloud.nongshim.co.kr/techblog/',
+                 'https://mail.google.com/mail/u/0/#inbox',
+                 'https://syundev.tistory.com/29?category=868616',
+                 'https://github.com/hotire/turnover-story',
+                 'http://www.bloter.net/archives/257437',
+                 'https://www.youtube.com/watch?v=r6TFnNQsQLY&feature=youtu.be',
+                 'https://ofcourse.kr/css-course/cursor-%EC%86%8D%EC%84%B1',
+                 'https://material.io/design/layout/responsive-layout-grid.html',
+                 'https://www.kobaco.co.kr/site/main/content/what_public_ad']:
         html = c.get_html(path)
         pprint(c.parse_html(html))
