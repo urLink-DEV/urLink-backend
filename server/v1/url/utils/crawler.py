99a2641613f2ac5a20e160a785e671155413141f
조홍제 <chj@wemakeprice.com>
2021-01-12 20:53:41 +0900

    크롤러 리팩토링

--- server/v1/url/utils/crawler.py
+++ server/v1/url/utils/crawler.py
@@ -9,22 +9,31 @@ from bs4 import BeautifulSoup
 class Crawler:
     IMAGE_404 = 'https://urlink.s3.ap-northeast-2.amazonaws.com/static/404-image.png'
     IMAGE_FAVICON = 'https://urlink.s3.ap-northeast-2.amazonaws.com/static/favicon.png'
+    HEADERS = {
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+        'Sec-Fetch-Dest': 'document'
+    }
 
-    async def get_html(self, path):
+    async def get_html_by_async(self, path):
         self.path = path
-        headers = {
-            'Connection': 'keep-alive',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-            'Sec-Fetch-Dest': 'document'
-        }
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get(self.path, headers=headers, timeout=5) as response:
+                async with session.get(self.path, headers=Crawler.HEADERS, timeout=5) as response:
                     response.encoding = None
                     return await response.text()
+        except Exception as e:
+            return str(e)
+
+    def get_html_by_sync(self, path):
+        self.path = path
+        try:
+            response = requests.get(self.path, headers=Crawler.HEADERS, timeout=5)
+            response.encoding = None
+            return response.text
         except Exception as e:
             return str(e)
 
@@ -49,16 +58,7 @@ class Crawler:
             print(e)
         return False
 
-    def is_unknown_image_or_favicon(self, type, path):
-        if path == "알수없음":
-            if type == 'image':
-                return self.IMAGE_404
-            else:
-                return self.IMAGE_FAVICON
-        else:
-            return path
-
-    def is_insufficient_url(self, path):
+    def attach_full_scheme_on_path(self, path):
         if urlparse(path).scheme == '':
             url = urlparse(self.path)
             if urlparse(path).netloc == '':
@@ -68,6 +68,18 @@ class Crawler:
         else:
             return path
 
+    def check_path(self, type, path):
+        default_path_dict = {"image": Crawler.IMAGE_404, "favicon": Crawler.IMAGE_FAVICON}
+
+        if path == "알수없음":
+            return default_path_dict[type]
+        else:
+            path = self.attach_full_scheme_on_path(path)
+            if self.is_correct_image(path):
+                return path
+            else:
+                return default_path_dict[type]
+
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         head = soup.head
@@ -76,8 +88,8 @@ class Crawler:
             return {
                 'title': '알수없음',
                 'description': '알수없음',
-                'image_path': self.IMAGE_404,
-                'favicon_path': self.IMAGE_FAVICON
+                'image_path': Crawler.IMAGE_404,
+                'favicon_path': Crawler.IMAGE_FAVICON
             }
 
         if head.title:
@@ -85,15 +97,8 @@ class Crawler:
         else:
             title = self.get_meta_data(head, [{'name': 'title'}, {'property': 'og:title'}])
         description = self.get_meta_data(head, [{'name': 'description'}, {'property': 'og:description'}])
-        image_path = self.is_insufficient_url(self.is_unknown_image_or_favicon('image', self.get_meta_data(head, [
-            {'property': 'og:image'}, {'property': 'twitter:image'}])))
-        favicon_path = self.is_insufficient_url(self.is_unknown_image_or_favicon('favicon', self.get_favicon(head, [
-            {'rel': 'shortcut icon'}, {'rel': 'icon'}])))
-
-        if not self.is_correct_image(image_path):
-            image_path = self.is_unknown_image_or_favicon('image', '알수없음')
-        if not self.is_correct_image(favicon_path):
-            favicon_path = self.is_unknown_image_or_favicon('favicon', '알수없음')
+        image_path = self.check_path("image", self.get_meta_data(head, [{'property': 'og:image'}, {'property': 'twitter:image'}]))
+        favicon_path = self.check_path("favicon", self.get_favicon(head, [{'rel': 'shortcut icon'}, {'rel': 'icon'}]))
 
         return {
             'title': title,
@@ -105,7 +110,7 @@ class Crawler:
 
 async def main():
     c = Crawler()
-    for i in [c.parse_html(i) for i in (await asyncio.gather(*[c.get_html(url) for url in urls]))]:
+    for i in [c.parse_html(i) for i in (await asyncio.gather(*[c.get_html_by_async(url) for url in urls]))]:
         pprint(i)
 
 
@@ -113,19 +118,43 @@ if __name__ == "__main__":
     from pprint import pprint
     import time
 
+    # 1. async
+    print("###async start###")
     start = time.time()
-    urls = [
-        'https://programmers.co.kr/learn/challenges?tab=all_challenges',
-        'https://www.acmicpc.net/',
-        'https://ssungkang.tistory.com/category/%EC%9B%B9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/Django',
-        'https://tech.cloud.nongshim.co.kr/techblog/',
-        'https://mail.google.com/mail/u/0/#inbox',
-        'https://syundev.tistory.com/29?category=868616',
-        'https://github.com/hotire/turnover-story',
-        'http://www.bloter.net/archives/257437',
-        'https://www.youtube.com/watch?v=r6TFnNQsQLY&feature=youtu.be',
-        'https://ofcourse.kr/css-course/cursor-%EC%86%8D%EC%84%B1',
-        'https://material.io/design/layout/responsive-layout-grid.html',
-        'https://www.kobaco.co.kr/site/main/content/what_public_ad']
+    urls = ['abc',
+            'https://programmers.co.kr/learn/challenges?tab=all_challenges',
+            'https://www.acmicpc.net/',
+            'https://ssungkang.tistory.com/category/%EC%9B%B9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/Django',
+            'https://tech.cloud.nongshim.co.kr/techblog/',
+            'https://mail.google.com/mail/u/0/#inbox',
+            'https://syundev.tistory.com/29?category=868616',
+            'https://github.com/hotire/turnover-story',
+            'http://www.bloter.net/archives/257437',
+            'https://www.youtube.com/watch?v=r6TFnNQsQLY&feature=youtu.be',
+            'https://ofcourse.kr/css-course/cursor-%EC%86%8D%EC%84%B1',
+            'https://material.io/design/layout/responsive-layout-grid.html',
+            'https://www.kobaco.co.kr/site/main/content/what_public_ad']
     asyncio.run(main())
-    print(time.time() - start)
+    print("async : ", time.time() - start)
+
+    # 2. sync
+    time.sleep(5)
+    print("###sync start###")
+    start = time.time()
+    c = Crawler()
+    for path in ['abc',
+                 'https://programmers.co.kr/learn/challenges?tab=all_challenges',
+                 'https://www.acmicpc.net/',
+                 'https://ssungkang.tistory.com/category/%EC%9B%B9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/Django',
+                 'https://tech.cloud.nongshim.co.kr/techblog/',
+                 'https://mail.google.com/mail/u/0/#inbox',
+                 'https://syundev.tistory.com/29?category=868616',
+                 'https://github.com/hotire/turnover-story',
+                 'http://www.bloter.net/archives/257437',
+                 'https://www.youtube.com/watch?v=r6TFnNQsQLY&feature=youtu.be',
+                 'https://ofcourse.kr/css-course/cursor-%EC%86%8D%EC%84%B1',
+                 'https://material.io/design/layout/responsive-layout-grid.html',
+                 'https://www.kobaco.co.kr/site/main/content/what_public_ad']:
+        html = c.get_html_by_sync(path)
+        pprint(c.parse_html(html))
+    print("sync : ", time.time() - start)
